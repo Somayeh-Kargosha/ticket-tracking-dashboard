5c7958174c6fed99f88c7ded30ec692859f3b433
Somayeh <somayehkargosha@gmail.com>
2025-03-10 09:22:51 +0330

    Create UserCard component

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,4 +1,34 @@
-const UserCard = () => {
-  return <div>user</div>;
+import React from "react";
+import "./UserCard.scss";
+
+const UserCard: React.FC = () => {
+  return (
+    <div className="user-card-container">
+      <div className="user-card">
+        <div className="avatar-container">
+          <img
+            src="src/assets/images/avatar.png"
+            alt="User Avatar"
+            className="avatar"
+          />
+        </div>
+        <div>
+          <p>Report for</p>
+          <div className="user-title">
+            <h2>Stacey</h2>
+            <h2>Castillo</h2>
+          </div>
+        </div>
+      </div>
+      <div className="timeframe-options">
+        <div className="btn-container">
+          {["daily", "weekly", "monthly"].map((tf) => (
+            <button key={tf}>{tf}</button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
+
 export default UserCard;
